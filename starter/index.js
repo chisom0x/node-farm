284916fe94d09b69c39d51f7dc47e99fbc9b368e
chisom0x <onyenankiekelvin@gmail.com>
2023-02-09 02:03:33 -0800

    modified:   starter/index.js 	modified:   starter/templates/overview.html 	modified:   starter/templates/product.html 	new file:   starter/templates/template-card.html

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//READING AND WRITING FILES WITH THE FILE SYSTEM(FS) MODULE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // blocking synchronously way 
 
@@ -11,12 +17,78 @@ const fs = require('fs');
 
 // non-blocking asynchronously way
 
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2)
-     })
-     fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
-        console.log(data3)
-     })
-})
-console.log('will read this...')
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2)
+//      })
+//      fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
+//         console.log(data3)
+//      })
+// })
+// console.log('will read this...')
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//BUILDING A HTTP SERVER
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const tempOverview = fs.readFileSync(
+   `${__dirname}/templates/overview.html`,
+   'utf-8'
+ );
+ const tempCard = fs.readFileSync(
+   `${__dirname}/templates/template-card.html`,
+   'utf-8'
+ );
+ const tempProduct = fs.readFileSync(
+   `${__dirname}/templates/product.html`,
+   'utf-8'
+ );
+ 
+ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+ const dataObj = JSON.parse(data);
+ 
+ const slugs = dataObj.map(el => (el.productName, { lower: true }));
+ console.log(slugs);
+ 
+ const server = http.createServer((req, res) => {
+   const { query, pathname } = url.parse(req.url, true);
+ 
+   // Overview page
+   if (pathname === '/' || pathname === '/overview') {
+     res.writeHead(200, {
+       'Content-type': 'text/html'
+     });
+ 
+     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+     res.end(output);
+ 
+     // Product page
+   } else if (pathname === '/product') {
+     res.writeHead(200, {
+       'Content-type': 'text/html'
+     });
+     const product = dataObj[query.id];
+     const output = replaceTemplate(tempProduct, product);
+     res.end(output);
+ 
+     // API
+   } else if (pathname === '/api') {
+     res.writeHead(200, {
+       'Content-type': 'application/json'
+     });
+     res.end(data);
+ 
+     // Not found
+   } else {
+     res.writeHead(404, {
+       'Content-type': 'text/html',
+       'my-own-header': 'hello-world'
+     });
+     res.end('<h1>Page not found!</h1>');
+   }
+ });
+ 
+ server.listen(8000, '127.0.0.1', () => {
+   console.log('Listening to requests on port 8000');
+ });
